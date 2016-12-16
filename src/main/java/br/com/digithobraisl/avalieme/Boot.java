13cf57ae20c3eb0f575b6a8b7cfa5d64f0ed8482
Vinícius Saueia <vsaueia@gmail.com>
2016-12-16 08:59:56 -0300

    Setup

---
+++ src/main/java/br/com/digithobraisl/avalieme/Boot.java
@@ -0,0 +1,25 @@
+package br.com.digithobraisl.avalieme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+@Controller
+public class Boot
+{
+
+   public static void main(String[] args)
+   {
+      SpringApplication.run(Boot.class, args);
+   }
+
+   @RequestMapping("/")
+   @ResponseBody
+   public String home()
+   {
+      return "home";
+   }
+}
