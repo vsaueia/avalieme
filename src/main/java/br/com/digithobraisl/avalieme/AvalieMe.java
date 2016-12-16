ecefa1609b1e66182a9d74a52001ad8207244188
Vinícius Saueia <vsaueia@gmail.com>
2016-12-16 15:08:44 -0300

    Correcao da url para buscar questionario do renan

--- src/main/java/br/com/digithobraisl/avalieme/AvalieMe.java
+++ src/main/java/br/com/digithobraisl/avalieme/AvalieMe.java
@@ -15,7 +15,7 @@ public class AvalieMe {
     @RequestMapping("/avaliacao")
     public List<Avaliacao> avaliacao(@RequestParam(value = "nome") String nome) throws IOException {
         RestTemplate restTemplate = new RestTemplate();
-        String consumeJSONString = restTemplate.getForObject("https://api.typeform.com/v1/form/bCqaHo?key=e2091ac4b93b4bfd26494c50c97a0e3bd96d5cdb&completed=true", String.class);
+        String consumeJSONString = restTemplate.getForObject("https://api.typeform.com/v1/form/RJRIvz?key=e2091ac4b93b4bfd26494c50c97a0e3bd96d5cdb&completed=true", String.class);
         TypeFormResponse typeFormResponse = new ObjectMapper().readValue(consumeJSONString, TypeFormResponse.class);
         return new Conversor(typeFormResponse).obterResultados(nome);
     }
